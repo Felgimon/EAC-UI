ee3372bb0cd3219b81ff0463bebd7eba21aa7992
Felgi <felipeferraridoval@gmail.com>
2025-05-14 15:39:23 -0300

    Par Impar

---
+++ Assets/Scripts/ParImpar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ParImpar : MonoBehaviour
+{
+    public TMP_InputField num1;
+    public TextMeshProUGUI result;
+
+    private void Start()
+    {
+        result.text = string.Empty;
+    }
+
+    public void esPar()
+    {
+        if (float.Parse(num1.text) % 2 == 0)
+        {
+            result.text = "El número es par";
+            return;
+        }
+        result.text = "El número es impar";
+    }
+}
