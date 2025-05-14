927dd7448949bdfe67de078a080e05c8834f6870
Felgi <felipeferraridoval@gmail.com>
2025-05-14 15:56:59 -0300

    ifs para checkear errores

--- Assets/Scripts/ParImpar.cs
+++ Assets/Scripts/ParImpar.cs
@@ -5,8 +5,9 @@ using TMPro;
 
 public class ParImpar : MonoBehaviour
 {
-    public TMP_InputField num1;
+    public TMP_InputField inputNum1;
     public TextMeshProUGUI result;
+    float num1;
 
     private void Start()
     {
@@ -15,7 +16,18 @@ public class ParImpar : MonoBehaviour
 
     public void esPar()
     {
-        if (float.Parse(num1.text) % 2 == 0)
+        if (inputNum1.text ==  "")
+        {
+            result.text = "El campo está vacío";
+            return;
+        }
+        num1 = float.Parse(inputNum1.text);
+        if (num1 <= 0)
+        {
+            result.text = "El número tiene que ser mayor a 0";
+            return;
+        }
+        if (num1 % 2 == 0)
         {
             result.text = "El número es par";
             return;
